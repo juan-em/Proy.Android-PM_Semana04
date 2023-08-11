aa6dd14cfcfd230ddc4669439dfccce7ec579922
juan-em <9jescobarm6@gmail.com>
2023-08-10 19:50:08 -0500

    commit

---
+++ app/src/main/java/com/miempresa/propuestos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.miempresa.propuestos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ep_2)
+    }
+}
